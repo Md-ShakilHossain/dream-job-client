ce418ca63eba6fc14efa8be882a780ef2842b9e4
Md-ShakilHossain <shakilh550@gmail.com>
2023-11-05 19:56:02 +0600

    Slider responsive issue solved

--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -5,31 +5,34 @@ const Slider = () => {
 
     const handleSearch = e => {
         e.preventDefault();
+        const form = e.target;
+        const searchText = form.searchText.value;
+        console.log(searchText);
     }
 
     return (
-        <div className="h-64 md:h-80 lg:h-[500px]">
+        <div className="h-64 md:h-80 rounded-lg lg:h-[500px]">
             <Carousel>
                 <div className="flex h-full items-center
                  justify-center">
-                    <img src="https://i.ibb.co/02sQ0DK/slider1.jpg" alt="" />
+                    <img src="https://i.ibb.co/8rhbXxs/slider1.jpg" className="h-64 md:h-80 w-full rounded-lg lg:h-[500px]" alt="" />
 
                 </div>
                 <div className="flex h-full items-center justify-center">
-                    <img src="https://i.ibb.co/bdZVKBQ/slider2.jpg" alt="" />
+                    <img src="https://i.ibb.co/Hh6TV67/slider2.jpg" className="h-64 md:h-80 w-full rounded-lg lg:h-[500px]" alt="" />
                 </div>
                 <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                    <img src="https://i.ibb.co/3CqR2tF/slider3.jpg" alt="" />
+                    <img src="https://i.ibb.co/rxJQ6zT/slider3.jpg" className="h-64 md:h-80 w-full rounded-lg lg:h-[500px]" alt="" />
                 </div>
             </Carousel>
-            <div className="bg-gradient-to-r from-black to-[#80000000] absolute w-11/12 h-[500px] top-20 rounded-lg p-40">
-                <div className="w-96">
-                    <h3 className="text-4xl font-bold text-white">Your Path to Success</h3>
-                    <p className="text-white font-semibold mt-4">Discover your dream job with seamless applications and expert guidance on our platform</p>
+            <div className="bg-gradient-to-r from-black to-[#80000000] absolute w-11/12 h-64 md:h-80 lg:h-[500px] top-[74px] md:top-20 rounded-lg p-2 md:p-10 lg:p-40">
+                <div className="w-64 md:w-96">
+                    <h3 className="text-2xl md:text-4xl font-bold text-white">Your Path to Success</h3>
+                    <p className="text-white md:font-semibold mt-2 md:mt-4">Discover your dream job with seamless applications and expert guidance on our platform</p>
                     <form onSubmit={handleSearch}>
-                        <input type="text" name="searchText" placeholder="Search By Job Title" id="" className="w-full rounded-2xl mt-8 text-center" />
+                        <input type="text" name="searchText" placeholder="Search By Job Title" id="" className="w-full rounded-2xl mt-4 md:mt-8 text-center" />
                         <div className="w-1/2 mt-2 mx-auto">
-                            <input className="bg-slate-300 py-1 rounded-2xl text-xl font-bold hover:bg-slate-400 w-full" type="submit" value="Search" />
+                            <input className="bg-slate-300 md:py-1 rounded-2xl text-xl font-bold hover:bg-slate-400 w-full" type="submit" value="Search" />
                         </div>
                     </form>
                 </div>
